59376926834ee393c0fafd9af87e1bba885ea450
Darshit Vora <dvvrocks@gmail.com>
2020-10-07 21:04:46 +0530

    Initial Commit

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,18 +1,25 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import {HitCounter} from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // Define an AWS lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler',{
+      runtime: lambda.Runtime.NODEJS_10_X,   // Uses NodeJS as runtime
+      code: lambda.Code.fromAsset('lambda'),  //Code is loaded from lambda directory
+      handler: 'hello.handler'      // handler function code filename.handler function
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter',{
+        downstream: hello
+      });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
   }
 }
